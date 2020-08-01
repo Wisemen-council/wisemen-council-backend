cdb2e8cd7a163a12d8810005a86317a797f07324
Juan Carlos Arocha <arochajuan2@gmail.com>
2020-08-01 13:30:30 -0400

    chore: initial Spring boot structure

---
+++ src/main/kotlin/com/wisemencouncil/app/WisemenCouncilBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.wisemencouncil.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WisemenCouncilBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<WisemenCouncilBackendApplication>(*args)
+}
