7ba1ce65e783603758a57d3fc8679a1adefffb6a
Daniel Arocha <arocha.daniel3@gmail.com>
2020-09-03 20:21:05 -0400

    test: add tests for user creation

--- src/test/kotlin/com/wisemencouncil/app/users/business/UserServiceTest.kt
+++ src/test/kotlin/com/wisemencouncil/app/users/business/UserServiceTest.kt
@@ -3,6 +3,7 @@ package com.wisemencouncil.app.users.business
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -49,5 +50,45 @@ internal class UserServiceTest {
         val actual = userService.findByEmail(AN_EMAIL)
 
         assertThat(actual).isNull()
+    }
+
+    private val A_NAME = "Jon Doe"
+    private val A_PASSWORD = "123456789"
+
+    private val A_SIGN_UP_DATA = SignUpData(AN_EMAIL, A_PASSWORD, A_NAME)
+    private val A_USER_NO_ID = mapSignUpDataToUserEntity(A_SIGN_UP_DATA)
+    private val A_USER = UserEntity(1L, AN_EMAIL, A_PASSWORD, A_NAME)
+
+    @Test
+    fun createUser_callsRepository() {
+        `when`(userRepository.save(A_USER_NO_ID)).thenReturn(A_USER)
+        userService.createUser(A_SIGN_UP_DATA)
+
+        Mockito.verify(userRepository).save(A_USER_NO_ID)
+    }
+
+    @Test
+    fun createUser_whenCreationSucceeds_returnsUser() {
+        `when`(userRepository.save(A_USER_NO_ID)).thenReturn(A_USER)
+        val user = userService.createUser(A_SIGN_UP_DATA)
+
+        assertThat(user).extracting("email", "password", "full_name")
+                .containsExactly(A_USER.email, A_USER.password, A_USER.full_name)
+    }
+
+    @Test
+    fun createUser_whenEmailIsInvalid_returnsSignUpException() {
+        assertThrows<SignUpException> {
+            val INVALID_SIGN_UP_DATA = SignUpData("invalid_email", A_PASSWORD, A_NAME)
+            userService.createUser(INVALID_SIGN_UP_DATA)
+        }
+    }
+
+    @Test
+    fun createUser_whenPasswordIsNotLongEnough_returnsSignUpException() {
+        assertThrows<SignUpException> {
+            val INVALID_SIGN_UP_DATA = SignUpData(AN_EMAIL, "", A_NAME)
+            userService.createUser(INVALID_SIGN_UP_DATA)
+        }
     }
 }
