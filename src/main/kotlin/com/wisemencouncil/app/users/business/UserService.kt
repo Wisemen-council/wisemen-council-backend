edf60c14876d777efcada3648725831da8105a07
Daniel Arocha <arocha.daniel3@gmail.com>
2020-09-03 20:20:09 -0400

    feat: create user method for users service

--- src/main/kotlin/com/wisemencouncil/app/users/business/UserService.kt
+++ src/main/kotlin/com/wisemencouncil/app/users/business/UserService.kt
@@ -1,11 +1,12 @@
 package com.wisemencouncil.app.users.business
 
+import com.wisemencouncil.app.security.isSignUpDataValid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserService @Autowired constructor(private val userRepository: UserRepository){
+class UserService @Autowired constructor(private val userRepository: UserRepository) {
 
     @Transactional
     fun findByEmail(email: String): User? {
@@ -15,6 +16,16 @@ class UserService @Autowired constructor(private val userRepository: UserReposit
             return entity
         }
 
+        return mapEntityToUser(entity)
+    }
+
+    @Transactional
+    fun createUser(signUpData: SignUpData): User? {
+        if (!isSignUpDataValid(signUpData)) {
+            throw SignUpException()
+        }
+
+        val entity = userRepository.save(mapSignUpDataToUserEntity(signUpData))
         return mapEntityToUser(entity)
     }
 }
