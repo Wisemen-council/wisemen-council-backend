fcc93a304210470dda8b848588f7eeb05179d430
Daniel Arocha <arocha.daniel3@gmail.com>
2020-09-03 20:19:21 -0400

    feat: add optional full_name to User data class

--- src/main/kotlin/com/wisemencouncil/app/users/business/User.kt
+++ src/main/kotlin/com/wisemencouncil/app/users/business/User.kt
@@ -1,3 +1,3 @@
 package com.wisemencouncil.app.users.business
 
-data class User(val id: Long, val email: String, val password: String)
+data class User(val id: Long, val email: String, val password: String, val full_name: String? = null)
