ef0fe15057efbe7aadfda162c5dbfa7dae43efbd
Daniel Arocha <arocha.daniel3@gmail.com>
2020-09-03 20:20:29 -0400

    feat: add user email and password verification utils

---
+++ src/main/kotlin/com/wisemencouncil/app/security/UserSignUpVerificationUtils.kt
@@ -0,0 +1,20 @@
+package com.wisemencouncil.app.security
+
+import com.wisemencouncil.app.users.business.SignUpData
+import java.util.regex.Pattern
+
+fun isEmailValid(email: String): Boolean {
+    return Pattern.compile(
+            "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                    + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                    + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+    ).matcher(email).matches()
+}
+
+fun isPasswordLongEnough(password: String): Boolean = password.length >= 8
+
+fun isSignUpDataValid(signUpData: SignUpData): Boolean =
+        isPasswordLongEnough(signUpData.password) && isEmailValid(signUpData.email)
