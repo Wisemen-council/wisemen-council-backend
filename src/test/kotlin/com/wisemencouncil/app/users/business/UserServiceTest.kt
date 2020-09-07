2bb07b11d72476c42ba76a4bae3997de1a07689c
Daniel Arocha <arocha.daniel3@gmail.com>
2020-09-06 23:08:13 -0300

    test: refactor user creation tests to use User clas

--- src/test/kotlin/com/wisemencouncil/app/users/business/UserServiceTest.kt
+++ src/test/kotlin/com/wisemencouncil/app/users/business/UserServiceTest.kt
@@ -55,31 +55,32 @@ internal class UserServiceTest {
     private val A_NAME = "Jon Doe"
     private val A_PASSWORD = "123456789"
 
-    private val A_SIGN_UP_DATA = SignUpData(AN_EMAIL, A_PASSWORD, A_NAME)
-    private val A_USER_NO_ID = mapSignUpDataToUserEntity(A_SIGN_UP_DATA)
-    private val A_USER = UserEntity(1L, AN_EMAIL, A_PASSWORD, A_NAME)
+    private val A_USER_NO_ID = User(null, AN_EMAIL, A_PASSWORD, A_NAME)
+    private val A_USER_ENTITY_NO_ID = UserEntity(null, AN_EMAIL, A_PASSWORD, A_NAME)
+
+    private val A_USER_ENTITY = UserEntity(1L, AN_EMAIL, A_PASSWORD, A_NAME)
 
     @Test
     fun createUser_callsRepository() {
-        `when`(userRepository.save(A_USER_NO_ID)).thenReturn(A_USER)
-        userService.createUser(A_SIGN_UP_DATA)
+        `when`(userRepository.save(A_USER_ENTITY_NO_ID)).thenReturn(A_USER_ENTITY)
+        userService.createUser(A_USER_NO_ID)
 
-        Mockito.verify(userRepository).save(A_USER_NO_ID)
+        Mockito.verify(userRepository).save(A_USER_ENTITY_NO_ID)
     }
 
     @Test
     fun createUser_whenCreationSucceeds_returnsUser() {
-        `when`(userRepository.save(A_USER_NO_ID)).thenReturn(A_USER)
-        val user = userService.createUser(A_SIGN_UP_DATA)
+        `when`(userRepository.save(A_USER_ENTITY_NO_ID)).thenReturn(A_USER_ENTITY)
+        val user = userService.createUser(A_USER_NO_ID)
 
         assertThat(user).extracting("email", "password", "full_name")
-                .containsExactly(A_USER.email, A_USER.password, A_USER.full_name)
+                .containsExactly(A_USER_ENTITY.email, A_USER_ENTITY.password, A_USER_ENTITY.full_name)
     }
 
     @Test
     fun createUser_whenEmailIsInvalid_returnsSignUpException() {
         assertThrows<SignUpException> {
-            val INVALID_SIGN_UP_DATA = SignUpData("invalid_email", A_PASSWORD, A_NAME)
+            val INVALID_SIGN_UP_DATA = User(null, "invalid_email", A_PASSWORD, A_NAME)
             userService.createUser(INVALID_SIGN_UP_DATA)
         }
     }
@@ -87,7 +88,7 @@ internal class UserServiceTest {
     @Test
     fun createUser_whenPasswordIsNotLongEnough_returnsSignUpException() {
         assertThrows<SignUpException> {
-            val INVALID_SIGN_UP_DATA = SignUpData(AN_EMAIL, "", A_NAME)
+            val INVALID_SIGN_UP_DATA = User(null, AN_EMAIL, "", A_NAME)
             userService.createUser(INVALID_SIGN_UP_DATA)
         }
     }
